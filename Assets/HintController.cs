975d6bdc355a9fca1ce717e2a0cd90e6ac75db5b
raytzou <raytzou@outlook.com>
2023-06-10 00:21:05 +0800

    Add hint controller

---
+++ Assets/HintController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class HintController : MonoBehaviour
+{
+    [SerializeField]
+    TMP_Text tmp;
+    void Start()
+    {
+        tmp = GetComponent<TMP_Text>();
+        StartCoroutine(FadeOutCoroutine());
+    }
+
+    private IEnumerator FadeOutCoroutine()
+    {
+        float duration = 2f; // 3 seconds
+        float currentTime = 0f;
+
+        while(currentTime < duration)
+        {
+            /// public static float Lerp(float a, float b, float t);
+            /// Linearly interpolates between a and b by t.
+            float textAlpha = Mathf.Lerp(1f, 0f, currentTime / duration);
+
+            tmp.color = new Color(tmp.color.r, tmp.color.g, tmp.color.b, textAlpha);
+            currentTime += Time.deltaTime;
+
+            yield return null;
+        }
+
+        yield break;
+    }
+}
