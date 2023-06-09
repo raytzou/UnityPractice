6f64f04243718daf6b6fa106f7951e574307b648
raytzou <raytzou@outlook.com>
2023-06-10 01:30:39 +0800

    Update movement script

--- Assets/Scripts/MyPlayerController.cs
+++ Assets/Scripts/MyPlayerController.cs
@@ -10,53 +10,39 @@ public class MyPlayerController : MonoBehaviour
     [SerializeField]
     [Range(0, 200)]
     float speed = 10f;
-    bool IsJump = false;
 
+    [SerializeField]
+    float jumpForce = 200f;
 
-    void Update() // IDK real-time here?
+    Camera camera;
+
+    private bool IsJump = false;
+
+    private void Start()
+    {
+        camera = GetComponent<Camera>();
+    }
+
+    private void Update() // IDK, does Update mean real-time here?
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
             IsJump = true;
-
         }
 
-        /*Input.GetAxis("Vertical");
-        Input.GetAxis("Horizontal");*/
-
-        if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
-        {
-            transform.position += speed * Time.deltaTime * Vector3.forward;
-        }
-        
-        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
-        {
-            transform.position += speed * Time.deltaTime * Vector3.left;
-        }
-
-        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
-        {
-            transform.position += speed * Time.deltaTime * Vector3.right;
-        }
-
-        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
-        {
-            transform.position += speed * Time.deltaTime * Vector3.back;
-        }
+        float xValue = Input.GetAxis("Horizontal");
+        float zValue = Input.GetAxis("Vertical");
+        transform.Translate(xValue * speed * Time.deltaTime, 0f, zValue * speed * Time.deltaTime);
     }
 
-    void FixedUpdate() // 物理運算會於此執行
+    private void FixedUpdate() // 物理運算會於此執行
     {
         //rigidBody.velocity = Vector3.zero;
 
         if(IsJump)
         {
-            
-            rigidBody.AddForce(Vector3.up * 100, ForceMode.Acceleration);
-            //transform.position += Vector3.up * 100 * Time.deltaTime;
+            rigidBody.AddForce(Vector3.up * jumpForce, ForceMode.Acceleration);
             IsJump = false; // 跳完直接切換
         }
-        
-        
     }
 }
