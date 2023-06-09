ac44d7bfe7b5bf06156ad77b9523ca91af5daf24
raytzou <raytzou@outlook.com>
2023-06-10 03:26:17 +0800

    Update player controller

--- Assets/Scripts/MyPlayerController.cs
+++ Assets/Scripts/MyPlayerController.cs
@@ -2,6 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+ * knowing bug: 
+ *  yaw cause rotation being weird
+ *  player can go through object sometime
+ * **/
+
 public class MyPlayerController : MonoBehaviour
 {
     [SerializeField]
@@ -14,35 +20,78 @@ public class MyPlayerController : MonoBehaviour
     [SerializeField]
     float jumpForce = 200f;
 
-    Camera camera;
+    [SerializeField]
+    [Range(1, 20)]
+    float mouseSpeed = 3f;
 
     private bool IsJump = false;
+    private bool IsEnableToJump = false;
+    private float mouseYaw = 0f, mousePitch = 0f;
 
-    private void Start()
+    private void Update() // IDK, does Update mean real-time?
     {
-        camera = GetComponent<Camera>();
-    }
-
-    private void Update() // IDK, does Update mean real-time here?
-    {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && IsEnableToJump)
         {
             IsJump = true;
         }
 
-        float xValue = Input.GetAxis("Horizontal");
-        float zValue = Input.GetAxis("Vertical");
-        transform.Translate(xValue * speed * Time.deltaTime, 0f, zValue * speed * Time.deltaTime);
+        PlayerMove();
+        MouseMove();
+        PlayerEdgeProtection();
     }
 
-    private void FixedUpdate() // 物理運算會於此執行
+    private void OnCollisionStay(Collision collision)
     {
-        //rigidBody.velocity = Vector3.zero;
+        
+        if (collision.transform.tag == "Ground")
+        {
+            IsEnableToJump = true;
+        }
+    }
 
+    private void OnCollisionExit(Collision collision)
+    {
+        if(IsEnableToJump)
+            IsEnableToJump = false;
+    }
+
+    private void FixedUpdate() // 物理運算於此運算似乎較精準?
+    {
         if(IsJump)
         {
             rigidBody.AddForce(Vector3.up * jumpForce, ForceMode.Acceleration);
             IsJump = false; // 跳完直接切換
+        }
+    }
+
+    private void PlayerMove()
+    {
+        float xValue = Input.GetAxis("Horizontal");
+        float zValue = Input.GetAxis("Vertical");
+
+        transform.Translate(xValue * speed * Time.deltaTime, 0f, zValue * speed * Time.deltaTime);
+    }
+
+    private void MouseMove()
+    {
+        int offset = 50;
+
+        mouseYaw += speed * offset * Time.deltaTime * Input.GetAxis("Mouse X");
+        mousePitch -= speed * offset * Time.deltaTime * Input.GetAxis("Mouse Y");
+
+        // restrict angle
+        if (mousePitch < -70) mousePitch = -70;
+        if (mousePitch > 80) mousePitch = 80;
+
+        transform.Find("Camera").GetComponent<Camera>().transform.eulerAngles = new Vector3(mousePitch, mouseYaw, 0f);
+        transform.eulerAngles = new Vector3(0f, mouseYaw, 0f);
+    }
+
+    private void PlayerEdgeProtection()
+    {
+        if(transform.position.y < -10)
+        {
+            transform.position = new Vector3(1f, 1f, 1f);
         }
     }
 }
