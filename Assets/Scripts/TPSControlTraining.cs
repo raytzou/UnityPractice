7f5f100e99d30d675ebc8aca0cd134bb10b26284
raytzou <raytzou@outlook.com>
2023-06-20 19:14:39 +0800

    Update: fixed TPS camera angle over the wall and ground

--- Assets/Scripts/TPSControlTraining.cs
+++ Assets/Scripts/TPSControlTraining.cs
@@ -15,6 +15,7 @@ public class TPSControlTraining : MonoBehaviour
 
     public TPSCameraTarget CameraTarget;
     public CharacterController Controller;
+    public LayerMask LayerMask; // for SphereCast
     public float Velocity = 5f;
 
     public Transform CameraTransform;
@@ -39,18 +40,36 @@ public class TPSControlTraining : MonoBehaviour
         float mouseX = Input.GetAxis("Mouse X");
         float mouseY = Input.GetAxis("Mouse Y"); // -1 to 1
 
+        #region 處理水平視角
         HorizontalDirection = Quaternion.Euler(0, mouseX * MouseSensitive, 0) * HorizontalDirection; // 水平旋轉
         //var newCameraPosition = CameraTarget.transform.position - HorizontalDirection * Distance; // 水平旋轉結果
+        #endregion
 
+        #region 處理垂直視角
         VerticalDegree += mouseY * MouseSensitive;
         //Debug.LogError(VerticalDegree); //mouse up = positive degree
         if (VerticalDegree > 80f) VerticalDegree = 80f;
-        else if(VerticalDegree < -80f) VerticalDegree = -80f;
+        else if (VerticalDegree < -80f) VerticalDegree = -80f;
         Vector3 horizontalAxis = Vector3.Cross(Vector3.up, HorizontalDirection); // 水平方向與全域座標上方互相外積，求得水平軸，用來與垂直度數計算向量軸的四元數
         var finalVector = Quaternion.AngleAxis(VerticalDegree, horizontalAxis) * HorizontalDirection; // 最後結果之向量
+        #endregion
 
-        var newCameraPosition = CameraTarget.transform.position - finalVector * DistanceToTarget; // 真正攝影機定位
 
+        //var newCameraPosition = CameraTarget.transform.position - finalVector * DistanceToTarget; // 處理過水平、垂直後的攝影機定位
+        var newCameraPosition = Vector3.zero; // 重新初始化
+
+
+        #region 處理鏡頭超出牆壁或地上
+        /// SphereCast(Target出發點，半徑，攝影鏡頭方向，raycast hit，鏡頭和Target的距離再加上微調，LayerMask)
+        if (Physics.SphereCast(CameraTarget.transform.position, 0.2f, -finalVector, out RaycastHit rayHit, DistanceToTarget + 0.2f, LayerMask))
+        {
+            newCameraPosition = CameraTarget.transform.position - finalVector * (rayHit.distance - 0.1f); // 計算Camera到Target距離
+        }
+        else
+        {
+            newCameraPosition = CameraTarget.transform.position - finalVector * DistanceToTarget;
+        }
+        #endregion
 
         CameraTransform.position = newCameraPosition;
         CameraTransform.LookAt(CameraTarget.transform.position);
