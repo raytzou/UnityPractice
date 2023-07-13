931aaf7836af9f9f5258d088057e1144bc0f963c
raytzou <raytzou@outlook.com>
2023-07-13 12:50:38 +0800

    cheange variable name

--- Assets/Scripts/ScoreUI.cs
+++ Assets/Scripts/ScoreUI.cs
@@ -5,18 +5,14 @@ using UnityEngine;
 public class ScoreUI : MonoBehaviour
 {
     [SerializeField] TMP_Text scoreUI;
-    private int score;
+    private int _score = 0;
 
-    private void Start()
-    {
-        score = 0;
-    }
     private void Update()
     {
-        score = GameObject.Find("Player").GetComponent<CalcScore>().score;
-        scoreUI.text = ("Score: " + score);
+        _score = GameObject.Find("Player").GetComponent<CalcScore>().PlayerScore;
+        scoreUI.text = ("Score: " + _score);
 
-        if(score == GameObject.FindGameObjectsWithTag("Star").Length)
+        if(_score == GameObject.FindGameObjectsWithTag("Star").Length)
         {
             scoreUI.text = "All Stars have been found!";
         }
