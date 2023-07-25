956a5ec96c33bd85eb7013c0555a2e82955e87d6
raytzou <raytzou@outlook.com>
2023-07-26 02:55:47 +0800

    fix star counts

--- Assets/Scripts/ScoreUI.cs
+++ Assets/Scripts/ScoreUI.cs
@@ -6,13 +6,19 @@ public class ScoreUI : MonoBehaviour
 {
     [SerializeField] TMP_Text scoreUI;
     private int _score = 0;
+    int starCounts;
+
+    private void Start()
+    {
+        starCounts = GameObject.FindGameObjectsWithTag("Star").Length;
+    }
 
     private void Update()
     {
         _score = GameObject.Find("Player").GetComponent<CalcScore>().PlayerScore;
         scoreUI.text = ("Score: " + _score);
 
-        if(_score == GameObject.FindGameObjectsWithTag("Star").Length)
+        if(_score == starCounts)
         {
             scoreUI.text = "All Stars have been found!";
         }
